ec73113f5a5ec78917cf42facac06279e2b3089e
DinaAdib <dina962008@hotmail.com>
2019-05-14 15:40:04 +0200

    Add files via upload

---
+++ Features/WikiFrequency.py
@@ -0,0 +1,11 @@
+from functions import *
+
+class WikiFrequency:
+    def __init__(self):
+        self.wiki_frequencies = load_obj(DIRECTORY+"wikiFreq")
+
+    def get_feature(self, phrase):
+        phrase = phrase.lower()
+        if phrase in self.wiki_frequencies:
+            return self.wiki_frequencies[phrase]
+        return 0
